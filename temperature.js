15d7bb4ff20989ad82f6b5d3006e2024b3694eab
Szymik, Lukasz <lukasz.szymik@hybris.com>
2016-01-01 22:55:41 +0100

    Adding sending temperature to ls-smarthome

--- temperature.js
+++ temperature.js
@@ -1,40 +1,52 @@
 var ds18b20 = require('ds18b20');
-var RADIATOR = '28-031467bacdff';
-var ROOM = '28-0115621a55ff';
-
-var config = require('config');
 var ip = require("ip");
+var config = require('config');
 
-var sensors = {};
-sensors[ROOM] = 22.7;
-sensors[RADIATOR] = 44.3;
 
-function readSensors(){
-  ds18b20.sensors(function (err, ids) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    ids.forEach(function (id) {
-          setInterval(function () {
-            readTemp(id);
-          }, 60000);
-        }
-    );
-  });
+var token = config.get('TOKEN')
+var smarthome_url = config.get('SMARTHOME_URL')
+var homeId = config.get('HOME_ID')
+
+
+function readSensors() {
+	ds18b20.sensors(function (err, ids) {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		ids.forEach(function (id) {
+				setInterval(function () {
+					readTemp(id);
+				}, 60000);
+			}
+		);
+	});
 }
 
 function readTemp(id) {
-  ds18b20.temperature(id, function (err, value) {
-    if (err) {
-      return console.error(err);
-    }
-    console.log('Sensor %s temperature %s', id, value);
-    sensors[id] = value;
-  });
+	ds18b20.temperature(id, function (err, value) {
+		if (err) {
+			return console.error(err);
+		}
+		console.log('Sensor %s temperature %s', id, value);
+		sensors[id] = value;
+	});
 }
 
-console.log( ip.address() );
+function sendTemperature(sensorId, temperature) {
+	request.post(smarthome_url + '/homes' + homeId + '/temperature/sensors' + sensorId,
+		{auth: {basic: token}, json: {value: temperature}}, function (err, resp) {
+
+			if (err) {
+			return console.error(err);
+		}
+		if (resp.statusCode !== 201) {
+			return console.error("Temperature for sensor %s has not been sent. Response code %s", sensorId, resp.statusCode);
+		}
+	});
+}
+
+console.log(ip.address());
 
 readSensors();
 
